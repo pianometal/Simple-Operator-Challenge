786855e7f46472b7fa682e7ea719a1530ba7b09c
Kyle Lovely <kylesphere@gmail.com>
2020-11-25 14:25:54 -0500

    Initial Commit

---
+++ Simple Operator Challenge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simple Operator Challenge
+//
+//  Created by Kyle Lovely on 11/25/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
